dcf54ea20d522a03c2ac339238114306e6c40123
Matheus-Leall <111158186+Matheus-Leall@users.noreply.github.com>
2023-11-01 17:12:34 -0300

    Create Principal.js

---
+++ Principal.js
@@ -0,0 +1,16 @@
+public class Principal (
+public static void main (String[l args) (
+10
+Scanner 3c = new Scanner (System. in) r
+Pessoas pessoa = new Pessoas ();
+12
+System.out printin ("Qual seu Nome? ");
+String nome = sc.nextLine ();
+System.out.printin ("Qual é a sua idade?") :
+int idade = sc.nextInt ();
+pessoa.Set Nome (nome);
+pessoa.Set_ Idade (idade);
+20
+.
+System.out.printin("Olá " + pessoa. Get Nome () ) ;
+System.out.printin ("Sua Idade é: " + pessoa.Get Idade () + " Anos."):
