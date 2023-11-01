5fc147d4fc6deef2a3e040713ab885de90f08666
Matheus-Leall <111158186+Matheus-Leall@users.noreply.github.com>
2023-11-01 17:02:38 -0300

    Create Pessoas.java

---
+++ Pessoas.java
@@ -0,0 +1,20 @@
+public class Pessoas {
+   private String nome;
+   private int idade;
+
+   public String Get Nome (){ 
+       return this. nome;
+   }
+
+   public String Set Nome (String nome) {
+       return this.nome = nome;
+   }
+   
+   public int Get Idade () { 
+      return this.idade;
+   }
+    
+   public int Set_ Idade (int idade) {
+       return this.idade = idade;
+   }
+}
